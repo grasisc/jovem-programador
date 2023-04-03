e962ec071417144506583de285a1bc47a7bb59a0
grasisc <128435903+grasisc@users.noreply.github.com>
2023-04-03 19:59:35 -0300

    Add files via upload

---
+++ Atividade 01 - 03-04-23.pas
@@ -0,0 +1,12 @@
+Program Pzim ;
+	var
+   numero, soma : real;
+Begin
+	writeln (' Digite um número');
+	readln (numero);    
+	soma:= (numero + 1);
+	writeln (' O sucessor deste numero é:  ', soma);
+	
+   
+   
+End.
