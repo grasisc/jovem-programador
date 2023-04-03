815e2fbe34d33fdf68d19b12d60938cf6547d0dd
grasisc <128435903+grasisc@users.noreply.github.com>
2023-04-03 14:00:30 -0300

    Add files via upload

---
+++ programa 10.pas
@@ -0,0 +1,23 @@
+Program Pzim ;
+var
+	valorA, valorB, valorC, multiplicacao, divisao : real;
+	
+Begin
+   writeln ('Qual o Valor A');
+   readln (valorA);
+   writeln ('Qual o Valor B');
+   readln (valorB);
+   writeln ('Qual o Valor C');
+   readln (valorC);
+    multiplicacao:= (valorA*ValorC);
+    divisao:=(valorA*ValorC) / 2;
+    writeln ('A area do triangulo que tem A por base e C por altura é de  ', divisao); 
+    multiplicacao:= (ValorC * ValorC) * Pi ;
+		writeln ('A area do circulo de raio C é de  ', multiplicacao) ;  
+		multiplicacao:= (ValorB * ValorB);
+		writeln ('A area do quadrado que tem lado B é de  ', multiplicacao) ;  
+		
+   
+   
+   
+End.
