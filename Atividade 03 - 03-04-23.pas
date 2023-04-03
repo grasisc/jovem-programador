c58c38e62d701af92a8a353f56f47169ece307d1
grasisc <128435903+grasisc@users.noreply.github.com>
2023-04-03 20:16:07 -0300

    Add files via upload

---
+++ Atividade 03 - 03-04-23.pas
@@ -0,0 +1,18 @@
+Program Pzim ;
+	var
+	numero, multiplicacao: real;
+Begin
+   writeln ('Escolha um número');
+   readln (numero);
+   writeln ('A tabuada de', numero, 'é:');
+	 writeln (numero, 'x1:', numero*1);
+	 writeln (numero, 'x2:', numero*2);
+	 writeln (numero, 'x3:', numero*3);
+	 writeln (numero, 'x4:', numero*4);
+	 writeln (numero, 'x5:', numero*5);
+	 writeln (numero, 'x6:', numero*6);
+	 writeln (numero, 'x7:', numero*7);
+	 writeln (numero, 'x8:', numero*8);
+	 writeln (numero, 'x9:', numero*9);
+	 writeln (numero, 'x10:', numero*10);
+End.
