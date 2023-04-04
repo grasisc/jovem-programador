5a74ad470bb4ac87bbc520c384343d396216cee1
grasisc <128435903+grasisc@users.noreply.github.com>
2023-04-03 21:06:17 -0300

    Add files via upload

---
+++ Atividade 07 - 03-04-23.pas
@@ -0,0 +1,22 @@
+Program Pzim ;
+	var
+	soma,subtracao: real;
+Begin
+	writeln (' o resultado de 12x4 é:');
+	soma:= (12+12+12+12);
+	writeln (soma);
+	writeln (' o resultado de 23x11 é:');
+	soma:= (23+23+23+23+23+23+23+23+23+23+23);
+	writeln (soma);
+	writeln (' o resultado de 10/2 é:');
+	subtracao:=(10-5);
+	writeln (subtracao);
+	writeln (' o resultado de 175/7 é:');
+	subtracao:=(175-150);
+	writeln (subtracao);
+	writeln (' o resultado de 28x5 é:');
+	soma:= (28+28+28+28+28);
+	writeln (soma);
+	
+  
+End.
